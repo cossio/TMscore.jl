04f1531b9daa62b551041107d7f3c71953c2c6c3
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-12-03 23:39:55 +0100

    split, wait

--- src/TMscore.jl
+++ src/TMscore.jl
@@ -4,15 +4,18 @@ import TMscore_jll
 
 function tmscore(
     pdb1::AbstractString, pdb2::AbstractString;
-    outfmt=nothing, seq::Bool=false
+    outfmt=nothing, seq::Bool=false, split=nothing,
+    wait::Bool=true
 )
     cmd = `$(TMscore_jll.TMscore())`
     isnothing(outfmt) || `$cmd -outfmt $outfmt`
+    isnothing(split) || `$cmd -split $split`
     seq && `$cmd -seq`
 
     stdout = tempname()
     stderr = tempname()
     process = run(pipeline(`$cmd $pdb1 $pdb2`; stdout, stderr); wait=false)
+    wait && Base.wait(process)
 
     return (; process, stdout, stderr)
 end
